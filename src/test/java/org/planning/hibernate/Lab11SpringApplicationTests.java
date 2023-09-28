d22a82b60c40bacad62f1e00f77f42986ced8128
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2023-09-29 01:11:03 +0300

    Initial commit

---
+++ src/test/java/org/planning/hibernate/Lab11SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.planning.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
